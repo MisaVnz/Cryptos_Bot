046e77bbc6110db4cd3681808116ee4b2f2b76bb
gabrielbaute <gabrielbaute@gmail.com>
2025-07-03 10:29:33 -0400

    Módulo de configuración del servicio

---
+++ app/settings/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+class Config:
+    def __init__(self):
+        load_dotenv()
+        self.TELEGRAM_BOT_KEY = os.getenv("TELEGRAM_BOT_KEY")
+        self.LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+        self.API_URL = os.getenv("API_URL", "https://api.coingecko.com/api/v3/simple/price")
+        self.CURRENCIES = os.getenv("CURRENCIES", "bitcoin,ethereum,solana,binancecoin,trondao").split(",")
+        self.APP_NAME = os.getenv("APP_NAME", "Cryptos Bot")
