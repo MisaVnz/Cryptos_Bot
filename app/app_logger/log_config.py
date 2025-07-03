a94ef7db6b6c3e785dc604c1475960bfdbe2b9d0
gabrielbaute <gabrielbaute@gmail.com>
2025-07-03 10:55:02 -0400

    Se agrega un archivo de log en el directorio logs/

--- app/app_logger/log_config.py
+++ app/app_logger/log_config.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from datetime import datetime
+
 import logging
 import sys
 import io
@@ -5,13 +8,23 @@ import io
 
 class LoggerConfig:
     """Configura logging para ver errores."""
+    def __init__(self):
+        """Inicializa la configuración del logger."""
+        self.file = self.log_file
+        
+        # Asegura que el directorio de logs exista
+    logs_dir = Path("logs")
+    logs_dir.mkdir(exist_ok=True)
     
+    log_file = logs_dir / f"bot_{datetime.now().strftime('%Y-%m-%d')}.log"
+
     def setup_logging(self):
         """Configura el logger con formato y nivel de logging."""    
         logging.basicConfig(
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
             level=logging.INFO,
             handlers=[
+                logging.FileHandler(self.log_file, encoding='utf-8'),
                 logging.StreamHandler(stream=sys.stdout),
             ]
         )
