ca5e4677bc0de1ad7798b9dbd365820ea079b1d1
gabrielbaute <gabrielbaute@gmail.com>
2025-07-03 10:28:17 -0400

    Módulo de bot

---
+++ app/bot/bot.py
@@ -0,0 +1,36 @@
+from telegram.ext import Application, CommandHandler
+
+from settings import Config
+from app_logger import get_logger
+from bot.commands import start, eth, sol, bnb, trx, btc
+
+logger = get_logger("[Cryptos Bot]")
+
+TOKEN = Config().TELEGRAM_BOT_KEY  
+
+async def post_init(application: Application):
+    await application.bot.delete_webhook(drop_pending_updates=True)
+
+def main():
+    # Verifica que el token esté 
+    
+    logger.info("🔗 Iniciando el bot de Telegram...")
+
+    if not TOKEN:
+        logger.error("❌ No se encontró TELEGRAM_BOT_KEY en las variables de entorno")
+        raise ValueError("Token de Telegram no configurado")
+    else:
+        logger.info("✅ Token de Telegram encontrado")
+
+    application = Application.builder().token(TOKEN).post_init(post_init).build()
+
+    # Handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("BTC", btc))
+    application.add_handler(CommandHandler("ETH", eth))
+    application.add_handler(CommandHandler("SOL", sol))
+    application.add_handler(CommandHandler("BNB", bnb))
+    application.add_handler(CommandHandler("TRX", trx))
+
+    logger.info("Bot iniciado. Usa /start para ver las opciones")
+    application.run_polling()
